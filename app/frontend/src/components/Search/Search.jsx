d1343a409c3dfdbc1d69069444592b93af542aca
Monisha Krothapalli <mkrothapalli123@gmail.com>
2024-07-25 18:02:57 -0700

    fixed search results box

--- app/frontend/src/components/Search/Search.jsx
+++ app/frontend/src/components/Search/Search.jsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState} from 'react';
 import { FaSearch } from "react-icons/fa";
 import './Search.css';
 
 const Search = ({ chapters, setChapter }) => {
   const [query, setQuery] = useState(""); 
   const [filteredChapters, setFilteredChapters] = useState([]);
+
 
   const fetchData = async (value) => {
     if (value.trim() === "") {
@@ -25,8 +26,10 @@ const Search = ({ chapters, setChapter }) => {
   }
 
   const pickChapter = (index) => {
+    setQuery(filteredChapters[index][1]); 
     setChapter(filteredChapters[index][1]);
     console.log(filteredChapters[index][1]);
+    setFilteredChapters([]);
   }
 
   return (
